1e757c8487b9d16fb36be2ba9c54c1226c3abfde
Mário Lúcio <marioljunior84@gmail.com>
2022-04-30 11:08:05 -0300

    Carga Inicial

---
+++ ApiCurso/src/test/java/br/com/rest/ApiCurso/ApiCursoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rest.ApiCurso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
